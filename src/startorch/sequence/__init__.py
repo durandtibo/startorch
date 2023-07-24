f0211e78c4fbdf9ae5dd066e340fb0be617c4a16
Thibaut Durand <durand.tibo@gmail.com>
2023-07-23 19:21:29 -0700

    Add `RandUniformSequenceGenerator`

--- src/startorch/sequence/__init__.py
+++ src/startorch/sequence/__init__.py
@@ -1,5 +1,10 @@
 from __future__ import annotations
 
-__all__ = ["BaseSequenceGenerator", "setup_sequence_generator"]
+__all__ = [
+    "BaseSequenceGenerator",
+    "RandUniform",
+    "setup_sequence_generator",
+]
 
 from startorch.sequence.base import BaseSequenceGenerator, setup_sequence_generator
+from startorch.sequence.uniform import RandUniformSequenceGenerator as RandUniform
---
+++ src/startorch/sequence/uniform.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+__all__ = ["RandUniformSequenceGenerator"]
+
+from collections.abc import Generator
+
+from redcat import BatchedTensorSeq
+
+from startorch.random import rand_uniform
+from startorch.sequence.base import BaseSequenceGenerator
+from startorch.utils.conversion import to_tuple
+
+
+class RandUniformSequenceGenerator(BaseSequenceGenerator):
+    r"""Implements a sequence generator to generate sequences by sampling
+    values from a uniform distribution.
+
+    Args:
+    ----
+        low (float, optional): Specifies the minimum value
+            (inclusive). Default: ``0.0``
+        high (float, optional): Specifies the maximum value
+            (exclusive). Default: ``1.0``
+        feature_size (tuple or list or int, optional): Specifies the
+            feature size. Default: ``1``
+
+    Example usage:
+
+    .. code-block:: pycon
+
+        >>> import torch
+        >>> from startorch.sequence import RandUniform
+        >>> generator = RandUniform()
+        >>> generator.generate(seq_len=12, batch_size=4)  # doctest:+ELLIPSIS
+        tensor([[...]], batch_dim=0, seq_dim=1)
+    """
+
+    def __init__(
+        self,
+        low: float = 0.0,
+        high: float = 1.0,
+        feature_size: tuple[int, ...] | list[int] | int = 1,
+    ) -> None:
+        super().__init__()
+        self._low = float(low)
+        if high < low:
+            raise ValueError(f"high ({high}) has to be greater or equal to low ({low})")
+        self._high = float(high)
+        self._feature_size = to_tuple(feature_size)
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__qualname__}(low={self._low}, "
+            f"high={self._high}, feature_size={self._feature_size})"
+        )
+
+    def generate(
+        self, seq_len: int, batch_size: int = 1, rng: Generator | None = None
+    ) -> BatchedTensorSeq:
+        return BatchedTensorSeq(
+            rand_uniform(
+                size=(batch_size, seq_len) + self._feature_size,
+                low=self._low,
+                high=self._high,
+                generator=rng,
+            )
+        )
