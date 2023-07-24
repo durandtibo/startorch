31dd709808e15c22e50107807da769ec10c2cec7
Thibaut Durand <durand.tibo@gmail.com>
2023-07-23 23:33:07 -0700

    Add data type sequence generators (#24)

--- src/startorch/sequence/__init__.py
+++ src/startorch/sequence/__init__.py
@@ -4,8 +4,10 @@ __all__ = [
     "BaseSequenceGenerator",
     "BaseWrapperSequenceGenerator",
     "Constant",
+    "Float",
     "Full",
     "Linear",
+    "Long",
     "RandInt",
     "RandUniform",
     "setup_sequence_generator",
@@ -14,6 +16,8 @@ __all__ = [
 from startorch.sequence.base import BaseSequenceGenerator, setup_sequence_generator
 from startorch.sequence.constant import ConstantSequenceGenerator as Constant
 from startorch.sequence.constant import FullSequenceGenerator as Full
+from startorch.sequence.dtype import FloatSequenceGenerator as Float
+from startorch.sequence.dtype import LongSequenceGenerator as Long
 from startorch.sequence.linear import LinearSequenceGenerator as Linear
 from startorch.sequence.uniform import RandIntSequenceGenerator as RandInt
 from startorch.sequence.uniform import RandUniformSequenceGenerator as RandUniform
