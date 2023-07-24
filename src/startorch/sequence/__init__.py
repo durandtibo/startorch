dba21eb279dc0ff047126d4a75ba21ed82e39202
Thibaut Durand <durand.tibo@gmail.com>
2023-07-23 19:08:30 -0700

    Add `BaseSequenceGenerator` (#17)

---
+++ src/startorch/sequence/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+__all__ = ["BaseSequenceGenerator", "setup_sequence_generator"]
+
+from startorch.sequence.base import BaseSequenceGenerator, setup_sequence_generator
