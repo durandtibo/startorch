edf36d59a8fa942e236d556310397a28668d12d2
Thibaut Durand <durand.tibo@gmail.com>
2023-07-27 16:43:45 -0700

    Add uniform tensor generators

--- src/startorch/tensor/__init__.py
+++ src/startorch/tensor/__init__.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
 __all__ = [
+    "AsinhUniform",
     "BaseTensorGenerator",
     "BaseWrapperTensorGenerator",
     "Full",
+    "LogUniform",
+    "RandAsinhUniform",
+    "RandInt",
+    "RandLogUniform",
     "RandUniform",
     "Uniform",
     "setup_tensor_generator",
@@ -11,6 +16,11 @@ __all__ = [
 
 from startorch.tensor.base import BaseTensorGenerator, setup_tensor_generator
 from startorch.tensor.constant import FullTensorGenerator as Full
+from startorch.tensor.uniform import AsinhUniformTensorGenerator as AsinhUniform
+from startorch.tensor.uniform import LogUniformTensorGenerator as LogUniform
+from startorch.tensor.uniform import RandAsinhUniformTensorGenerator as RandAsinhUniform
+from startorch.tensor.uniform import RandIntTensorGenerator as RandInt
+from startorch.tensor.uniform import RandLogUniformTensorGenerator as RandLogUniform
 from startorch.tensor.uniform import RandUniformTensorGenerator as RandUniform
 from startorch.tensor.uniform import UniformTensorGenerator as Uniform
 from startorch.tensor.wrapper import BaseWrapperTensorGenerator
