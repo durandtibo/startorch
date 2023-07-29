1222c33e7db7d28fd44a07d36645543f7cc17acb
Thibaut Durand <durand.tibo@gmail.com>
2023-07-28 22:01:35 -0700

    Add `str_weighted_modules` (#68)

--- src/startorch/utils/format.py
+++ src/startorch/utils/format.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
-__all__ = ["str_target_object"]
+__all__ = ["str_target_object", "str_weighted_modules"]
 
+from collections.abc import Sequence
+
+from coola.utils import str_indent
 from objectory import OBJECT_TARGET
 
 
@@ -29,3 +32,39 @@ def str_target_object(config: dict) -> str:
         [_target_: N/A]
     """
     return f"[{OBJECT_TARGET}: {config.get(OBJECT_TARGET, 'N/A')}]"
+
+
+def str_weighted_modules(modules: Sequence, weights: Sequence, num_spaces: int = 2) -> str:
+    r"""Computes a pretty representation of a sequence of modules where
+    each module is associated to a weight.
+
+    Args:
+        modules (``Sequence``): Specifies the modules.
+        weights (``Sequence``): Specifies the weights. The ``weights``
+            should have the same length that ``modules``.
+        num_spaces (int, optional): Specifies the number of spaces
+            used for the indentation. Default: ``2``.
+
+    Returns:
+        str: The string representation of the modules.
+
+    Example usage:
+
+    .. code-block:: pycon
+
+        >>> from startorch.utils.format import str_weighted_modules
+        >>> print(str_weighted_modules(modules=["abc", "something\nelse"], weights=[1, 2]))
+        (0) [weight=1] abc
+        (1) [weight=2] something
+          else
+    """
+    if len(modules) != len(weights):
+        raise RuntimeError(
+            f"`modules` and `weights` must have the same length but received {len(modules):,} "
+            f"and {len(weights):,}"
+        )
+
+    lines = []
+    for i, (item, weight) in enumerate(zip(modules, weights)):
+        lines.append(f"({i}) [weight={weight}] {str_indent(str(item), num_spaces=num_spaces)}")
+    return "\n".join(lines)
--- tests/unit/utils/test_format.py
+++ tests/unit/utils/test_format.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 from objectory import OBJECT_TARGET
+from pytest import raises
 
-from startorch.utils.format import str_target_object
+from startorch.utils.format import str_target_object, str_weighted_modules
 
 #######################################
 #     Tests for str_target_object     #
@@ -17,3 +18,28 @@ def test_str_target_object_with_target() -> None:
 
 def test_str_target_object_without_target() -> None:
     assert str_target_object({}) == "[_target_: N/A]"
+
+
+##########################################
+#     Tests for str_weighted_modules     #
+##########################################
+
+
+def test_str_weighted_modules_empty() -> None:
+    assert str_weighted_modules([], []) == ""
+
+
+def test_str_weighted_modules_1() -> None:
+    assert str_weighted_modules(["abc"], [2]) == "(0) [weight=2] abc"
+
+
+def test_str_weighted_modules_2() -> None:
+    assert (
+        str_weighted_modules(["abc", "something\nelse"], [2, 1])
+        == "(0) [weight=2] abc\n(1) [weight=1] something\n  else"
+    )
+
+
+def test_str_weighted_modules_different_lengths() -> None:
+    with raises(RuntimeError, match="`modules` and `weights` must have the same length"):
+        str_weighted_modules(["abc"], [2, 3])
