01871d214e6e5eae5a6c5043511261746c90e67d
Thibaut Durand <durand.tibo@gmail.com>
2023-08-03 22:48:03 -0700

    Add `BasePeriodicSequenceGenerator`

---
+++ src/startorch/periodic/sequence/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+__all__ = ["BasePeriodicSequenceGenerator", "setup_periodic_sequence_generator"]
+
+from startorch.periodic.sequence.base import (
+    BasePeriodicSequenceGenerator,
+    setup_periodic_sequence_generator,
+)
