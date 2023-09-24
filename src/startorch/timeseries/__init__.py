b24a9c75cffe7a3df4042b165390003ddd917709
Thibaut Durand <durand.tibo@gmail.com>
2023-09-24 08:16:19 -0700

    Add `MultinomialChoiceTimeSeriesGenerator` (#142)

--- src/startorch/timeseries/__init__.py
+++ src/startorch/timeseries/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 __all__ = [
     "BaseTimeSeriesGenerator",
+    "MultinomialChoice",
     "TimeSeries",
     "merge_multiple_timeseries_by_time",
     "setup_timeseries_generator",
@@ -11,6 +12,9 @@ __all__ = [
 from startorch.timeseries.base import (
     BaseTimeSeriesGenerator,
     setup_timeseries_generator,
+)
+from startorch.timeseries.choice import (
+    MultinomialChoiceTimeSeriesGenerator as MultinomialChoice,
 )
 from startorch.timeseries.generic import TimeSeriesGenerator as TimeSeries
 from startorch.timeseries.merge import (
