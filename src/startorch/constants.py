9cda16894910c9735b66421a68309cd6eb8f6f05
Thibaut Durand <durand.tibo@gmail.com>
2023-09-26 21:06:08 -0700

    Add `create_hypercube_examples` (#157)

--- src/startorch/constants.py
+++ src/startorch/constants.py
@@ -1,7 +1,17 @@
-__all__ = ["BATCH_SIZE", "SEQUENCE", "SEQ_LEN", "TIME", "VALUE"]
+__all__ = [
+    "BATCH_SIZE",
+    "FEATURE",
+    "SEQUENCE",
+    "SEQ_LEN",
+    "TARGET",
+    "TIME",
+    "VALUE",
+]
 
 BATCH_SIZE = "batch_size"
+FEATURE = "feature"
 SEQUENCE = "sequence"
 SEQ_LEN = "seq_len"
+TARGET = "target"
 TIME = "time"
 VALUE = "value"
