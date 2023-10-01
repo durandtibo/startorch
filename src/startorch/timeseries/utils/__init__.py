ef9ce00984e9512718dfe87f67e029e5aafec305
Thibaut Durand <durand.tibo@gmail.com>
2023-09-30 19:45:08 -0700

    Add function to merge time series by time

---
+++ src/startorch/timeseries/utils/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+__all__ = ["merge_timeseries_by_time"]
+
+from startorch.timeseries.utils.merge import merge_timeseries_by_time
--- tests/unit/timeseries/utils/test_merge.py
+++ tests/unit/timeseries/utils/test_merge.py
@@ -4,7 +4,7 @@ from pytest import raises
 from redcat import BatchDict, BatchedTensorSeq, BatchList
 
 from startorch import constants as ct
-from startorch.timeseries.utils.merge import merge_timeseries_by_time
+from startorch.timeseries.utils import merge_timeseries_by_time
 
 #############################################
 #    Tests for merge_timeseries_by_time     #
@@ -49,7 +49,153 @@ def test_merge_timeseries_by_time_batch_size_1() -> None:
                 ),
             }
         ),
+    )
+
+
+def test_merge_timeseries_by_time_batch_size_2() -> None:
+    assert objects_are_equal(
+        merge_timeseries_by_time(
+            [
+                BatchDict(
+                    {
+                        ct.TIME: BatchedTensorSeq(
+                            torch.tensor(
+                                [[[5], [10], [15], [20], [25]], [[-5], [-10], [-15], [-20], [-25]]],
+                                dtype=torch.float,
+                            )
+                        ),
+                        ct.VALUE: BatchedTensorSeq(
+                            torch.tensor(
+                                [[11, 12, 13, 14, 15], [-11, -12, -13, -14, -15]], dtype=torch.long
+                            )
+                        ),
+                    }
+                ),
+                BatchDict(
+                    {
+                        ct.TIME: BatchedTensorSeq(
+                            torch.tensor(
+                                [[[6], [12], [16], [24]], [[-6], [-12], [-16], [-24]]],
+                                dtype=torch.float,
+                            )
+                        ),
+                        ct.VALUE: BatchedTensorSeq(
+                            torch.tensor([[21, 22, 23, 24], [-21, -22, -23, -24]], dtype=torch.long)
+                        ),
+                    }
+                ),
+            ]
+        ),
+        BatchDict(
+            {
+                ct.TIME: BatchedTensorSeq(
+                    torch.tensor(
+                        [
+                            [[5], [6], [10], [12], [15], [16], [20], [24], [25]],
+                            [[-25], [-24], [-20], [-16], [-15], [-12], [-10], [-6], [-5]],
+                        ],
+                        dtype=torch.float,
+                    )
+                ),
+                ct.VALUE: BatchedTensorSeq(
+                    torch.tensor(
+                        [
+                            [11, 21, 12, 22, 13, 23, 14, 24, 15],
+                            [-15, -24, -14, -23, -13, -22, -12, -21, -11],
+                        ],
+                        dtype=torch.long,
+                    )
+                ),
+            }
+        ),
+    )
+
+
+def test_merge_timeseries_by_time_time_feature_0d() -> None:
+    assert objects_are_equal(
+        merge_timeseries_by_time(
+            [
+                BatchDict(
+                    {
+                        ct.TIME: BatchedTensorSeq(
+                            torch.tensor(
+                                [[5, 10, 15, 20, 25], [4, 8, 12, 16, 20]], dtype=torch.float
+                            )
+                        ),
+                        ct.VALUE: BatchedTensorSeq(
+                            torch.tensor(
+                                [[111, 112, 113, 114, 115], [121, 122, 123, 124, 125]],
+                                dtype=torch.long,
+                            )
+                        ),
+                    }
+                ),
+                BatchDict(
+                    {
+                        ct.TIME: BatchedTensorSeq(
+                            torch.tensor([[6, 12, 16, 24], [3, 6, 9, 12]], dtype=torch.float)
+                        ),
+                        ct.VALUE: BatchedTensorSeq(
+                            torch.tensor(
+                                [[211, 212, 213, 214], [221, 222, 223, 224]], dtype=torch.long
+                            )
+                        ),
+                    }
+                ),
+            ]
+        ),
+        BatchDict(
+            {
+                ct.TIME: BatchedTensorSeq(
+                    torch.tensor(
+                        [[5, 6, 10, 12, 15, 16, 20, 24, 25], [3, 4, 6, 8, 9, 12, 12, 16, 20]],
+                        dtype=torch.float,
+                    )
+                ),
+                ct.VALUE: BatchedTensorSeq(
+                    torch.tensor(
+                        [
+                            [111, 211, 112, 212, 113, 213, 114, 214, 115],
+                            [221, 121, 222, 122, 223, 123, 224, 124, 125],
+                        ],
+                        dtype=torch.long,
+                    )
+                ),
+            }
+        ),
         show_difference=True,
+    )
+
+
+def test_merge_timeseries_by_time_only_time_feature() -> None:
+    assert objects_are_equal(
+        merge_timeseries_by_time(
+            [
+                BatchDict(
+                    {
+                        ct.TIME: BatchedTensorSeq(
+                            torch.tensor([[[5], [10], [15], [20], [25]]], dtype=torch.float)
+                        ),
+                    }
+                ),
+                BatchDict(
+                    {
+                        ct.TIME: BatchedTensorSeq(
+                            torch.tensor([[[6], [12], [16], [24]]], dtype=torch.float)
+                        ),
+                    }
+                ),
+            ]
+        ),
+        BatchDict(
+            {
+                ct.TIME: BatchedTensorSeq(
+                    torch.tensor(
+                        [[[5], [6], [10], [12], [15], [16], [20], [24], [25]]], dtype=torch.float
+                    )
+                ),
+            }
+        ),
     )
 
 
