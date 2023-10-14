d14f920a730cb5a353421c5fe5e51ab6d3598865
Thibaut Durand <durand.tibo@gmail.com>
2023-10-13 23:31:01 -0700

    Add `LinearRegressionExampleGenerator`

--- src/startorch/example/__init__.py
+++ src/startorch/example/__init__.py
@@ -4,12 +4,14 @@ __all__ = [
     "BaseExampleGenerator",
     "Hypercube",
     "HypercubeExampleGenerator",
+    "LinearRegression",
+    "LinearRegressionExampleGenerator",
     "SwissRoll",
     "SwissRollExampleGenerator",
     "TimeSeries",
     "TimeSeriesExampleGenerator",
     "is_example_generator_config",
-    "make_normal_linear_regression",
+    "make_linear_regression",
     "make_swiss_roll",
     "setup_example_generator",
 ]
@@ -21,7 +23,11 @@ from startorch.example.base import (
 )
 from startorch.example.hypercube import HypercubeExampleGenerator
 from startorch.example.hypercube import HypercubeExampleGenerator as Hypercube
-from startorch.example.regression import make_normal_linear_regression
+from startorch.example.regression import LinearRegressionExampleGenerator
+from startorch.example.regression import (
+    LinearRegressionExampleGenerator as LinearRegression,
+)
+from startorch.example.regression import make_linear_regression
 from startorch.example.swissroll import SwissRollExampleGenerator
 from startorch.example.swissroll import SwissRollExampleGenerator as SwissRoll
 from startorch.example.swissroll import make_swiss_roll
