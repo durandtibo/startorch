3b51465862f0cf09299836d95130190993e1a7a2
Thibaut Durand <durand.tibo@gmail.com>
2023-10-20 23:29:10 -0700

    Add `check_std` (#217)

--- src/startorch/example/utils/__init__.py
+++ src/startorch/example/utils/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-__all__ = ["check_num_examples"]
+__all__ = ["check_num_examples", "check_std"]
 
-from startorch.example.utils.validation import check_num_examples
+from startorch.example.utils.validation import check_num_examples, check_std
--- tests/unit/example/utils/test_validation.py
+++ tests/unit/example/utils/test_validation.py
@@ -2,7 +2,7 @@ from typing import Any
 
 from pytest import mark, raises
 
-from startorch.example.utils import check_num_examples
+from startorch.example.utils import check_num_examples, check_std
 
 ########################################
 #     Tests for check_num_examples     #
@@ -22,9 +22,60 @@ def test_check_num_examples_incorrect_type(value: Any) -> None:
         check_num_examples(value)
 
 
+def test_check_num_examples_incorrect_type_custom_name() -> None:
+    with raises(TypeError, match="Incorrect type for n_samples. Expected an integer but received"):
+        check_num_examples(1.2, name="n_samples")
+
+
 @mark.parametrize("value", (0, -1))
 def test_check_num_examples_incorrect_value(value: int) -> None:
     with raises(
         RuntimeError, match="Incorrect value for num_examples. Expected a value greater than 0"
     ):
         check_num_examples(value)
+
+
+def test_check_num_examples_incorrect_value_custom_name() -> None:
+    with raises(
+        RuntimeError, match="Incorrect value for n_samples. Expected a value greater than 0"
+    ):
+        check_num_examples(0, "n_samples")
+
+
+###############################
+#     Tests for check_std     #
+###############################
+
+
+@mark.parametrize("value", (1, 1.2, 2))
+def test_check_std_valid(value: int) -> None:
+    check_std(value)
+
+
+@mark.parametrize("value", ("abc", None))
+def test_check_std_incorrect_type(value: Any) -> None:
+    with raises(
+        TypeError, match="Incorrect type for std. Expected an integer or float but received"
+    ):
+        check_std(value)
+
+
+def test_check_std_incorrect_type_custom_name() -> None:
+    with raises(
+        TypeError,
+        match="Incorrect type for noise_std. Expected an integer or float but received",
+    ):
+        check_std("-1", name="noise_std")
+
+
+@mark.parametrize("value", (-1, -4.2))
+def test_check_std_incorrect_value(value: int) -> None:
+    with raises(RuntimeError, match="Incorrect value for std. Expected a value greater than 0"):
+        check_std(value)
+
+
+def test_check_std_incorrect_value_custom_name() -> None:
+    with raises(
+        RuntimeError, match="Incorrect value for noise_std. Expected a value greater than 0"
+    ):
+        check_std(-1, name="noise_std")
