d6349fd5ba3ca90c67735ad12d47e067458498b6
Thibaut Durand <durand.tibo@gmail.com>
2023-10-20 22:08:26 -0700

    Add `make_circles_classification` (#214)

---
+++ src/startorch/example/circles.py
@@ -0,0 +1,106 @@
+from __future__ import annotations
+
+__all__ = ["make_circles_classification"]
+
+import math
+
+import torch
+from redcat import BatchDict, BatchedTensor
+
+from startorch import constants as ct
+from startorch.random import rand_normal
+
+
+def make_circles_classification(
+    num_examples: int = 100,
+    shuffle: bool = True,
+    noise_std: float = 0.0,
+    factor: float = 0.8,
+    ratio: float = 0.5,
+    generator: torch.Generator | None = None,
+) -> BatchDict[BatchedTensor]:
+    r"""Generates a binary classification dataset where the data are
+    generated with a large circle containing a smaller circle in 2d.
+
+    The implementation is based on
+    https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_circles.html
+
+    Args:
+    ----
+        num_examples (int, optional): Specifies the number of examples.
+            Default: ``100``
+        shuffle (bool, optional): If ``True``, the examples are
+            shuffled. Default: ``True``
+        noise_std (float, optional): Specifies the standard deviation
+            of the Gaussian noise. Default: ``0.0``
+        factor (float, optional): Specifies the scale factor between
+            inner and outer circle in the range ``[0, 1)``.
+            Default: ``0.8``
+        ratio (float, optional): Specifies the ratio between the
+            number of examples in outer circle and inner circle.
+            Default: ``0.5``
+        generator (``torch.Generator`` or ``None``, optional):
+            Specifies an optional random generator. Default: ``None``
+
+    Returns:
+    -------
+        ``BatchDict``: A batch with two items:
+            - ``'input'``: a ``BatchedTensor`` of type float and
+                shape ``(num_examples, 2)``. This
+                tensor represents the input features.
+            - ``'target'``: a ``BatchedTensor`` of type long and
+                shape ``(num_examples,)``. This tensor represents
+                the targets.
+
+    Raises:
+    ------
+        RuntimeError if one of the parameters is not valid.
+
+    Example usage:
+
+    .. code-block:: pycon
+
+        >>> from startorch.example import make_circles_classification
+        >>> batch = make_circles_classification(num_examples=10)
+        >>> batch
+        BatchDict(
+          (target): tensor([...], batch_dim=0)
+          (feature): tensor([[...]], batch_dim=0)
+        )
+    """
+    if num_examples < 1:
+        raise RuntimeError(f"The number of examples ({num_examples}) has to be greater than 0")
+    if noise_std < 0:
+        raise RuntimeError(
+            f"The standard deviation of the Gaussian noise ({noise_std}) has to be "
+            "greater or equal than 0"
+        )
+    if factor < 0.0 or factor >= 1.0:
+        raise RuntimeError(f"Incorrect factor ({factor}). factor has to be in the range [0, 1)")
+
+    num_examples_out = math.ceil(num_examples * ratio)
+    num_examples_in = num_examples - num_examples_out
+
+    linspace_out = torch.linspace(0, 2 * math.pi, num_examples_out)
+    linspace_in = torch.linspace(0, 2 * math.pi, num_examples_in)
+    outer_circ = torch.stack([linspace_out.cos(), linspace_out.sin()], dim=1)
+    inner_circ = torch.stack([linspace_in.cos() * factor, linspace_in.sin() * factor], dim=1)
+
+    features = torch.cat([outer_circ, inner_circ], dim=0)
+    targets = torch.cat(
+        [
+            torch.zeros(num_examples_out, dtype=torch.long),
+            torch.ones(num_examples_in, dtype=torch.long),
+        ],
+        dim=0,
+    )
+
+    if noise_std > 0.0:
+        features += rand_normal(
+            size=(num_examples_out + num_examples_in, 2), std=noise_std, generator=generator
+        )
+
+    batch = BatchDict({ct.TARGET: BatchedTensor(targets), ct.FEATURE: BatchedTensor(features)})
+    if shuffle:
+        batch.shuffle_along_batch_(generator)
+    return batch
---
+++ tests/unit/example/test_circles.py
@@ -0,0 +1,131 @@
+from __future__ import annotations
+
+import torch
+from coola import objects_are_equal
+from pytest import mark, raises
+from redcat import BatchDict, BatchedTensor
+
+from startorch import constants as ct
+from startorch.example import make_circles_classification
+from startorch.utils.seed import get_torch_generator
+
+SIZES = (1, 2, 4)
+
+
+#################################################
+#     Tests for make_circles_classification     #
+#################################################
+
+
+@mark.parametrize("num_examples", (0, -1))
+def test_make_circles_classification_incorrect_num_examples(num_examples: int) -> None:
+    with raises(RuntimeError, match="The number of examples .* has to be greater than 0"):
+        make_circles_classification(num_examples=num_examples)
+
+
+def test_make_circles_classification_incorrect_noise_std() -> None:
+    with raises(
+        RuntimeError,
+        match="The standard deviation of the Gaussian noise .* has to be greater or equal than 0",
+    ):
+        make_circles_classification(noise_std=-1)
+
+
+def test_make_circles_classification() -> None:
+    data = make_circles_classification(num_examples=100)
+    assert isinstance(data, BatchDict)
+    assert len(data) == 2
+    targets = data[ct.TARGET]
+    assert isinstance(targets, BatchedTensor)
+    assert targets.batch_size == 100
+    assert targets.shape == (100,)
+    assert targets.dtype == torch.long
+    assert targets.sum() == 50
+    assert targets.min() >= 0
+    assert targets.max() <= 1
+
+    features = data[ct.FEATURE]
+    assert isinstance(data[ct.FEATURE], BatchedTensor)
+    assert features.batch_size == 100
+    assert features.shape == (100, 2)
+    assert features.dtype == torch.float
+    assert features.min() >= -1.0
+    assert features.max() <= 1.0
+
+
+def test_make_circles_classification_shuffle_false() -> None:
+    data = make_circles_classification(num_examples=10, shuffle=False)
+    assert isinstance(data, BatchDict)
+    assert len(data) == 2
+    assert data[ct.TARGET].equal(BatchedTensor(torch.tensor([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])))
+
+    features = data[ct.FEATURE]
+    assert isinstance(data[ct.FEATURE], BatchedTensor)
+    assert features.batch_size == 10
+    assert features.shape == (10, 2)
+    assert features.dtype == torch.float
+    assert features.min() >= -1.0
+    assert features.max() <= 1.0
+
+
+@mark.parametrize("factor", (0.2, 0.5, 0.8))
+def test_make_circles_classification_factor(factor: float) -> None:
+    data = make_circles_classification(num_examples=100, factor=factor)
+    assert isinstance(data, BatchDict)
+    assert len(data) == 2
+    targets = data[ct.TARGET]
+    assert isinstance(targets, BatchedTensor)
+    assert targets.batch_size == 100
+    assert targets.shape == (100,)
+    assert targets.dtype == torch.long
+    assert targets.sum() == 50
+    assert targets.min() >= 0
+    assert targets.max() <= 1
+
+    features = data[ct.FEATURE]
+    assert isinstance(data[ct.FEATURE], BatchedTensor)
+    assert features.batch_size == 100
+    assert features.shape == (100, 2)
+    assert features.dtype == torch.float
+    assert features.min() >= -1.0
+    assert features.max() <= 1.0
+
+
+@mark.parametrize("num_examples", SIZES)
+def test_make_circles_classification_num_examples(num_examples: int) -> None:
+    data = make_circles_classification(num_examples)
+    assert len(data) == 2
+    assert data[ct.TARGET].batch_size == num_examples
+    assert data[ct.FEATURE].batch_size == num_examples
+
+
+@mark.parametrize("noise_std", (0.0, 1.0))
+def test_make_circles_classification_same_random_seed(noise_std: float) -> None:
+    assert objects_are_equal(
+        make_circles_classification(
+            num_examples=64,
+            noise_std=noise_std,
+            generator=get_torch_generator(1),
+        ),
+        make_circles_classification(
+            num_examples=64,
+            noise_std=noise_std,
+            generator=get_torch_generator(1),
+        ),
+    )
+
+
+@mark.parametrize("noise_std", (0.0, 1.0))
+def test_make_circles_classification_different_random_seeds(noise_std: float) -> None:
+    assert not objects_are_equal(
+        make_circles_classification(
+            num_examples=64,
+            noise_std=noise_std,
+            generator=get_torch_generator(1),
+        ),
+        make_circles_classification(
+            num_examples=64,
+            noise_std=noise_std,
+            generator=get_torch_generator(2),
+        ),
+    )
