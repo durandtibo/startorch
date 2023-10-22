8f861623685ffc10f4a7e4bfa444065b2df4e355
Thibaut Durand <durand.tibo@gmail.com>
2023-10-22 10:43:02 -0700

    Add `CirclesClassificationExampleGenerator` (#225)

--- src/startorch/example/circles.py
+++ src/startorch/example/circles.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-__all__ = ["make_circles_classification"]
+__all__ = ["CirclesClassificationExampleGenerator", "make_circles_classification"]
 
 import math
 
@@ -8,8 +8,103 @@ import torch
 from redcat import BatchDict, BatchedTensor
 
 from startorch import constants as ct
+from startorch.example import BaseExampleGenerator
 from startorch.example.utils import check_interval, check_num_examples, check_std
 from startorch.random import rand_normal
+
+
+class CirclesClassificationExampleGenerator(BaseExampleGenerator[BatchedTensor]):
+    r"""Implements a manifold example generator based on the Swiss roll
+    pattern.
+
+    The implementation is based on
+    https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_circles.html
+
+    Args:
+    ----
+        shuffle (bool, optional): If ``True``, the examples are
+            shuffled. Default: ``True``
+        noise_std (float, optional): Specifies the standard deviation
+            of the Gaussian noise. Default: ``0.0``
+        factor (float, optional): Specifies the scale factor between
+            inner and outer circle in the range ``[0, 1)``.
+            Default: ``0.8``
+        ratio (float, optional): Specifies the ratio between the
+            number of examples in outer circle and inner circle.
+            Default: ``0.5``
+
+    Raises:
+    ------
+        TypeError or RuntimeError if one of the parameters is not
+            valid.
+
+    Example usage:
+
+    .. code-block:: pycon
+
+        >>> from startorch.example import CirclesClassification
+        >>> generator = CirclesClassification()
+        >>> generator
+        CirclesClassificationExampleGenerator(shuffle=True, noise_std=0.0, factor=0.8, ratio=0.5)
+        >>> batch = generator.generate(batch_size=10)
+        >>> batch
+        BatchDict(
+          (target): tensor([...], batch_dim=0)
+          (feature): tensor([[...]], batch_dim=0)
+        )
+    """
+
+    def __init__(
+        self,
+        shuffle: bool = True,
+        noise_std: float = 0.0,
+        factor: float = 0.8,
+        ratio: float = 0.5,
+    ) -> None:
+        self._shuffle = bool(shuffle)
+        check_std(noise_std, "noise_std")
+        self._noise_std = float(noise_std)
+
+        check_interval(factor, low=0.0, high=1.0, name="factor")
+        self._factor = float(factor)
+
+        check_interval(ratio, low=0.0, high=1.0, name="ratio")
+        self._ratio = float(ratio)
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__qualname__}(shuffle={self._shuffle}, "
+            f"noise_std={self._noise_std}, factor={self._factor}, ratio={self._ratio})"
+        )
+
+    @property
+    def factor(self) -> float:
+        r"""``float``: the scale factor between inner and outer
+        circle."""
+        return self._factor
+
+    @property
+    def noise_std(self) -> float:
+        r"""``float``: The standard deviation of the Gaussian noise."""
+        return self._noise_std
+
+    @property
+    def ratio(self) -> float:
+        r"""``float``: the ratio between the number of examples in outer
+        circle and inner circle."""
+        return self._ratio
+
+    def generate(
+        self, batch_size: int = 1, rng: torch.Generator | None = None
+    ) -> BatchDict[BatchedTensor]:
+        return make_circles_classification(
+            num_examples=batch_size,
+            shuffle=self._shuffle,
+            noise_std=self._noise_std,
+            factor=self._factor,
+            ratio=self._ratio,
+            generator=rng,
+        )
 
 
 def make_circles_classification(
--- tests/unit/example/test_circles.py
+++ tests/unit/example/test_circles.py
@@ -1,15 +1,114 @@
 from __future__ import annotations
 
+from unittest.mock import patch
+
 import torch
 from coola import objects_are_equal
 from pytest import mark, raises
 from redcat import BatchDict, BatchedTensor
 
 from startorch import constants as ct
-from startorch.example import make_circles_classification
+from startorch.example import CirclesClassification, make_circles_classification
 from startorch.utils.seed import get_torch_generator
 
 SIZES = (1, 2, 4)
+
+
+###########################################################
+#     Tests for CirclesClassificationExampleGenerator     #
+###########################################################
+
+
+def test_circles_classification_str() -> None:
+    assert str(CirclesClassification()).startswith("CirclesClassificationExampleGenerator(")
+
+
+@mark.parametrize("noise_std", (0, 0.1, 1))
+def test_circles_classification_noise_std(noise_std: float) -> None:
+    assert CirclesClassification(noise_std=noise_std).noise_std == noise_std
+
+
+def test_circles_classification_incorrect_noise_std() -> None:
+    with raises(
+        RuntimeError,
+        match="Incorrect value for noise_std. Expected a value greater than 0",
+    ):
+        CirclesClassification(noise_std=-1)
+
+
+@mark.parametrize("factor", (0.5, 0.8))
+def test_circles_classification_factor(factor: float) -> None:
+    assert CirclesClassification(factor=factor).factor == factor
+
+
+@mark.parametrize("factor", (-0.1, 1.0))
+def test_circles_classification_incorrect_factor(factor: float) -> None:
+    with raises(RuntimeError, match="Incorrect value for factor. Expected a value in interval"):
+        CirclesClassification(factor=factor)
+
+
+@mark.parametrize("batch_size", SIZES)
+def test_circles_classification_generate(batch_size: int) -> None:
+    data = CirclesClassification().generate(batch_size)
+    assert isinstance(data, BatchDict)
+    assert len(data) == 2
+    assert isinstance(data[ct.TARGET], BatchedTensor)
+    assert data[ct.TARGET].batch_size == batch_size
+    assert data[ct.TARGET].shape == (batch_size,)
+    assert data[ct.TARGET].dtype == torch.long
+    assert isinstance(data[ct.FEATURE], BatchedTensor)
+    assert data[ct.FEATURE].batch_size == batch_size
+    assert data[ct.FEATURE].shape == (batch_size, 2)
+    assert data[ct.FEATURE].dtype == torch.float
+
+
+@mark.parametrize("noise_std", (0.0, 1.0))
+@mark.parametrize("shuffle", (True, False))
+def test_circles_classification_generate_same_random_seed(
+    noise_std: float | int, shuffle: bool
+) -> None:
+    generator = CirclesClassification(noise_std=noise_std, shuffle=shuffle)
+    assert generator.generate(batch_size=64, rng=get_torch_generator(1)).equal(
+        generator.generate(batch_size=64, rng=get_torch_generator(1))
+    )
+
+
+@mark.parametrize("noise_std", (0.0, 1.0))
+def test_circles_classification_generate_different_random_seeds(noise_std: float | int) -> None:
+    generator = CirclesClassification(noise_std=noise_std)
+    assert not generator.generate(batch_size=64, rng=get_torch_generator(1)).equal(
+        generator.generate(batch_size=64, rng=get_torch_generator(2))
+    )
+
+
+@mark.parametrize(
+    "batch_size,noise_std,ratio,factor,shuffle,rng",
+    (
+        (2, 0.0, 0.2, 0.5, True, None),
+        (4, 0.5, 0.8, 0.5, False, get_torch_generator(1)),
+    ),
+)
+def test_circles_classification_generate_mock(
+    batch_size: int,
+    noise_std: float | int,
+    factor: float,
+    ratio: float,
+    shuffle: bool,
+    rng: torch.Generator | None,
+) -> None:
+    generator = CirclesClassification(
+        noise_std=noise_std, shuffle=shuffle, ratio=ratio, factor=factor
+    )
+    with patch("startorch.example.circles.make_circles_classification") as make_mock:
+        generator.generate(batch_size=batch_size, rng=rng)
+        make_mock.assert_called_once_with(
+            num_examples=batch_size,
+            shuffle=shuffle,
+            noise_std=noise_std,
+            factor=factor,
+            ratio=ratio,
+            generator=rng,
+        )
 
 
 #################################################
