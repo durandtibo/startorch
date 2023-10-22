e468f82783923076f0cddfcfb6dee1bd0cc9f8e1
Thibaut Durand <durand.tibo@gmail.com>
2023-10-22 13:19:13 -0700

    Add `MoonsClassificationExampleGenerator` (#227)

--- src/startorch/example/moons.py
+++ src/startorch/example/moons.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-__all__ = ["make_moons_classification"]
+__all__ = ["MoonsClassificationExampleGenerator", "make_moons_classification"]
 
 import math
 
@@ -8,8 +8,85 @@ import torch
 from redcat import BatchDict, BatchedTensor
 
 from startorch import constants as ct
+from startorch.example.base import BaseExampleGenerator
 from startorch.example.utils import check_interval, check_num_examples, check_std
 from startorch.random import rand_normal
+
+
+class MoonsClassificationExampleGenerator(BaseExampleGenerator[BatchedTensor]):
+    r"""Implements a binary classification example generator where the
+    data are generated with a large circle containing a smaller circle
+    in 2d.
+
+    The implementation is based on
+    https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_circles.html
+
+    Args:
+    ----
+        shuffle (bool, optional): If ``True``, the examples are
+            shuffled. Default: ``True``
+        noise_std (float, optional): Specifies the standard deviation
+            of the Gaussian noise. Default: ``0.0``
+        ratio (float, optional): Specifies the ratio between the
+            number of examples in outer circle and inner circle.
+            Default: ``0.5``
+
+    Raises:
+    ------
+        TypeError or RuntimeError if one of the parameters is not
+            valid.
+
+    Example usage:
+
+    .. code-block:: pycon
+
+        >>> from startorch.example import MoonsClassification
+        >>> generator = MoonsClassification()
+        >>> generator
+        MoonsClassificationExampleGenerator(shuffle=True, noise_std=0.0, ratio=0.5)
+        >>> batch = generator.generate(batch_size=10)
+        >>> batch
+        BatchDict(
+          (target): tensor([...], batch_dim=0)
+          (feature): tensor([[...]], batch_dim=0)
+        )
+    """
+
+    def __init__(self, shuffle: bool = True, noise_std: float = 0.0, ratio: float = 0.5) -> None:
+        self._shuffle = bool(shuffle)
+        check_std(noise_std, "noise_std")
+        self._noise_std = float(noise_std)
+
+        check_interval(ratio, low=0.0, high=1.0, name="ratio")
+        self._ratio = float(ratio)
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__qualname__}(shuffle={self._shuffle}, "
+            f"noise_std={self._noise_std}, ratio={self._ratio})"
+        )
+
+    @property
+    def noise_std(self) -> float:
+        r"""``float``: The standard deviation of the Gaussian noise."""
+        return self._noise_std
+
+    @property
+    def ratio(self) -> float:
+        r"""``float``: the ratio between the number of examples in outer
+        circle and inner circle."""
+        return self._ratio
+
+    def generate(
+        self, batch_size: int = 1, rng: torch.Generator | None = None
+    ) -> BatchDict[BatchedTensor]:
+        return make_moons_classification(
+            num_examples=batch_size,
+            shuffle=self._shuffle,
+            noise_std=self._noise_std,
+            ratio=self._ratio,
+            generator=rng,
+        )
 
 
 def make_moons_classification(
@@ -75,7 +152,7 @@ def make_moons_classification(
     linspace_out = torch.linspace(0, math.pi, num_examples_out)
     linspace_in = torch.linspace(0, math.pi, num_examples_in)
     outer_circ = torch.stack([linspace_out.cos(), linspace_out.sin()], dim=1)
-    inner_circ = torch.stack([linspace_in.cos().sub(1.0), linspace_in.sin().add(0.5)], dim=1)
+    inner_circ = torch.stack([1.0 - linspace_in.cos(), 0.5 - linspace_in.sin()], dim=1)
 
     features = torch.cat([outer_circ, inner_circ], dim=0)
     targets = torch.cat(
--- tests/unit/example/test_moons.py
+++ tests/unit/example/test_moons.py
@@ -1,15 +1,117 @@
 from __future__ import annotations
 
+from unittest.mock import patch
+
 import torch
 from coola import objects_are_equal
 from pytest import mark, raises
 from redcat import BatchDict, BatchedTensor
 
 from startorch import constants as ct
-from startorch.example import make_moons_classification
+from startorch.example import MoonsClassification, make_moons_classification
 from startorch.utils.seed import get_torch_generator
 
 SIZES = (1, 2, 4)
+
+
+#########################################################
+#     Tests for MoonsClassificationExampleGenerator     #
+#########################################################
+
+
+def test_moons_classification_str() -> None:
+    assert str(MoonsClassification()).startswith("MoonsClassificationExampleGenerator(")
+
+
+@mark.parametrize("noise_std", (0, 0.1, 1))
+def test_moons_classification_noise_std(noise_std: float) -> None:
+    assert MoonsClassification(noise_std=noise_std).noise_std == noise_std
+
+
+def test_moons_classification_incorrect_noise_std() -> None:
+    with raises(
+        RuntimeError,
+        match="Incorrect value for noise_std. Expected a value greater than 0",
+    ):
+        MoonsClassification(noise_std=-1)
+
+
+@mark.parametrize("ratio", (0.5, 0.8))
+def test_moons_classification_ratio(ratio: float) -> None:
+    assert MoonsClassification(ratio=ratio).ratio == ratio
+
+
+@mark.parametrize("ratio", (-0.1, 1.0))
+def test_moons_classification_incorrect_ratio(ratio: float) -> None:
+    with raises(RuntimeError, match="Incorrect value for ratio. Expected a value in interval"):
+        MoonsClassification(ratio=ratio)
+
+
+@mark.parametrize("batch_size", SIZES)
+def test_moons_classification_generate(batch_size: int) -> None:
+    data = MoonsClassification().generate(batch_size)
+    assert isinstance(data, BatchDict)
+    assert len(data) == 2
+    targets = data[ct.TARGET]
+    assert isinstance(targets, BatchedTensor)
+    assert targets.batch_size == batch_size
+    assert targets.shape == (batch_size,)
+    assert targets.dtype == torch.long
+    assert targets.min() >= 0
+    assert targets.max() <= 1
+
+    features = data[ct.FEATURE]
+    assert isinstance(data[ct.FEATURE], BatchedTensor)
+    assert features.batch_size == batch_size
+    assert features.shape == (batch_size, 2)
+    assert features.dtype == torch.float
+    assert features.min() >= -1.0
+    assert features.max() <= 2.0
+
+
+@mark.parametrize("noise_std", (0.0, 1.0))
+@mark.parametrize("shuffle", (True, False))
+def test_moons_classification_generate_same_random_seed(
+    noise_std: float | int, shuffle: bool
+) -> None:
+    generator = MoonsClassification(noise_std=noise_std, shuffle=shuffle)
+    assert generator.generate(batch_size=64, rng=get_torch_generator(1)).equal(
+        generator.generate(batch_size=64, rng=get_torch_generator(1))
+    )
+
+
+@mark.parametrize("noise_std", (0.0, 1.0))
+def test_moons_classification_generate_different_random_seeds(noise_std: float | int) -> None:
+    generator = MoonsClassification(noise_std=noise_std)
+    assert not generator.generate(batch_size=64, rng=get_torch_generator(1)).equal(
+        generator.generate(batch_size=64, rng=get_torch_generator(2))
+    )
+
+
+@mark.parametrize(
+    "batch_size,noise_std,ratio,shuffle,rng",
+    (
+        (2, 0.0, 0.5, True, None),
+        (4, 0.5, 0.2, False, get_torch_generator(1)),
+    ),
+)
+def test_moons_classification_generate_mock(
+    batch_size: int,
+    noise_std: float | int,
+    ratio: float,
+    shuffle: bool,
+    rng: torch.Generator | None,
+) -> None:
+    generator = MoonsClassification(noise_std=noise_std, shuffle=shuffle, ratio=ratio)
+    with patch("startorch.example.moons.make_moons_classification") as make_mock:
+        generator.generate(batch_size=batch_size, rng=rng)
+        make_mock.assert_called_once_with(
+            num_examples=batch_size,
+            shuffle=shuffle,
+            noise_std=noise_std,
+            ratio=ratio,
+            generator=rng,
+        )
 
 
 ###############################################
@@ -62,7 +164,7 @@ def test_make_moons_classification() -> None:
     assert features.batch_size == 100
     assert features.shape == (100, 2)
     assert features.dtype == torch.float
-    assert features.min() >= -2.0
+    assert features.min() >= -1.0
     assert features.max() <= 2.0
 
 
@@ -77,7 +179,7 @@ def test_make_moons_classification_shuffle_false() -> None:
     assert features.batch_size == 10
     assert features.shape == (10, 2)
     assert features.dtype == torch.float
-    assert features.min() >= -2.0
+    assert features.min() >= -1.0
     assert features.max() <= 2.0
 
 
@@ -92,7 +194,7 @@ def test_make_moons_classification_ratio_0_2() -> None:
     assert features.batch_size == 10
     assert features.shape == (10, 2)
     assert features.dtype == torch.float
-    assert features.min() >= -2.0
+    assert features.min() >= -1.0
     assert features.max() <= 2.0
 
 
