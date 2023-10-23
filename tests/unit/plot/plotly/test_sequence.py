4e542023373ab10a3ddf70eb1690644136a2f25f
Thibaut Durand <durand.tibo@gmail.com>
2023-10-22 20:43:36 -0700

    Add `plotly.plot_sequence` (#236)

--- tests/unit/plot/plotly/test_sequence.py
+++ tests/unit/plot/plotly/test_sequence.py
@@ -5,7 +5,7 @@ from unittest.mock import patch
 from coola import objects_are_equal
 from pytest import mark, raises
 
-from startorch.plot.plotly import hist_sequence
+from startorch.plot.plotly import hist_sequence, plot_sequence
 from startorch.sequence import BaseSequenceGenerator, RandNormal, RandUniform
 from startorch.testing import plotly_available
 from startorch.utils.imports import is_plotly_available
@@ -71,3 +71,60 @@ def test_hist_sequence_rng(seed: int) -> None:
 def test_hist_sequence_no_plotly() -> None:
     with raises(RuntimeError, match="`plotly` package is required but not installed."):
         hist_sequence(RandUniform())
+
+
+###################################
+#     Tests for plot_sequence     #
+###################################
+
+
+@plotly_available
+@mark.parametrize("generator", (RandUniform(), RandNormal()))
+def test_plot_sequence_generator(generator: BaseSequenceGenerator) -> None:
+    assert isinstance(plot_sequence(generator, batch_size=2, seq_len=6), go.Figure)
+
+
+@patch("startorch.utils.imports.is_plotly_available", lambda *args, **kwargs: True)
+@mark.parametrize("seq_len", (1, 2, 4))
+def test_plot_sequence_seq_len(seq_len: int) -> None:
+    with patch("startorch.plot.plotly.sequence.go") as go:
+        plot_sequence(RandUniform(), seq_len=seq_len)
+        assert go.Scatter.call_args.kwargs["x"].shape[0] == seq_len
+
+
+@patch("startorch.utils.imports.is_plotly_available", lambda *args, **kwargs: True)
+@mark.parametrize("batch_size", (1, 2, 4))
+def test_plot_sequence_batch_size(batch_size: int) -> None:
+    with patch("startorch.plot.plotly.sequence.go") as go:
+        plot_sequence(RandUniform(), batch_size=batch_size)
+        assert len(go.Scatter.call_args_list) == batch_size
+
+
+@patch("startorch.utils.imports.is_plotly_available", lambda *args, **kwargs: True)
+@mark.parametrize("num_batches", (1, 2, 4))
+def test_plot_sequence_num_batches(num_batches: int) -> None:
+    with patch("startorch.plot.plotly.sequence.go") as go:
+        plot_sequence(RandUniform(), batch_size=1, num_batches=num_batches)
+        assert len(go.Scatter.call_args_list) == num_batches
+
+
+@patch("startorch.utils.imports.is_plotly_available", lambda *args, **kwargs: True)
+@mark.parametrize("seed", (1, 2, 4))
+def test_plot_sequence_rng(seed: int) -> None:
+    with patch("startorch.plot.plotly.sequence.go") as go:
+        plot_sequence(RandUniform(), rng=seed)
+        plot_sequence(RandUniform(), rng=get_torch_generator(seed))
+        assert objects_are_equal(
+            go.Scatter.call_args_list[0].kwargs["x"],
+            go.Scatter.call_args_list[1].kwargs["x"],
+        )
+        assert objects_are_equal(
+            go.Scatter.call_args_list[0].kwargs["y"],
+            go.Scatter.call_args_list[1].kwargs["y"],
+        )
+
+
+@patch("startorch.utils.imports.is_plotly_available", lambda *args, **kwargs: False)
+def test_plot_sequence_no_plotly() -> None:
+    with raises(RuntimeError, match="`plotly` package is required but not installed."):
+        plot_sequence(RandUniform())
