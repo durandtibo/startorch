936dc034e18bfcf151867ed05f84685cc00aa833
Thibaut Durand <durand.tibo@gmail.com>
2024-05-26 08:27:31 -0700

    Add utility functions to `BaseTransitionGenerator` (#409)

--- src/startorch/transition/__init__.py
+++ src/startorch/transition/__init__.py
@@ -2,8 +2,18 @@ r"""Contain transition matrix generators."""
 
 from __future__ import annotations
 
-__all__ = ["BaseTransitionGenerator", "Diagonal", "DiagonalTransitionGenerator"]
+__all__ = [
+    "BaseTransitionGenerator",
+    "Diagonal",
+    "DiagonalTransitionGenerator",
+    "is_transition_generator_config",
+    "setup_transition_generator",
+]
 
-from startorch.transition.base import BaseTransitionGenerator
+from startorch.transition.base import (
+    BaseTransitionGenerator,
+    is_transition_generator_config,
+    setup_transition_generator,
+)
 from startorch.transition.diag import DiagonalTransitionGenerator
 from startorch.transition.diag import DiagonalTransitionGenerator as Diagonal
