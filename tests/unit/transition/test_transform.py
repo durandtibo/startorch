6ac14cd20e3594ab3bbffc60a3b5823991f4cd41
Thibaut Durand <durand.tibo@gmail.com>
2024-05-27 17:04:03 -0700

    Add `TransformTransitionGenerator`

--- tests/unit/transition/test_transform.py
+++ tests/unit/transition/test_transform.py
@@ -3,8 +3,9 @@ from __future__ import annotations
 import torch
 from coola import objects_are_equal
 
+from startorch.tensor import RandUniform
 from startorch.tensor.transformer import Clamp
-from startorch.transition import Diagonal, Transform
+from startorch.transition import Diagonal, TensorTransitionGenerator, Transform
 from startorch.utils.seed import get_torch_generator
 
 ##################################################
@@ -35,7 +36,9 @@ def test_transform_generate() -> None:
 
 
 def test_transform_generate_same_random_seed() -> None:
-    generator = Transform(Diagonal(), Clamp(min=0.0, max=0.5))
+    generator = Transform(
+        TensorTransitionGenerator(generator=RandUniform()), Clamp(min=0.0, max=0.5)
+    )
     assert objects_are_equal(
         generator.generate(n=9, rng=get_torch_generator(1)),
         generator.generate(n=9, rng=get_torch_generator(1)),
@@ -43,9 +46,10 @@ def test_transform_generate_same_random_seed() -> None:
 
 
 def test_transform_generate_different_random_seeds() -> None:
-    generator = Transform(Diagonal(), Clamp(min=0.0, max=0.5))
-    # they are equal because the generator does not have randomness
-    assert objects_are_equal(
+    generator = Transform(
+        TensorTransitionGenerator(generator=RandUniform()), Clamp(min=0.0, max=0.5)
+    )
+    assert not objects_are_equal(
         generator.generate(n=9, rng=get_torch_generator(1)),
         generator.generate(n=9, rng=get_torch_generator(2)),
     )
