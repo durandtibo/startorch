0dd8c1b0eac5edf5324393b4fa3b504c766f76d8
Thibaut Durand <durand.tibo@gmail.com>
2024-05-28 23:28:13 -0700

    Add trigo tensor transformers

--- src/startorch/tensor/transformer/__init__.py
+++ src/startorch/tensor/transformer/__init__.py
@@ -5,11 +5,23 @@ from __future__ import annotations
 __all__ = [
     "Abs",
     "AbsTensorTransformer",
+    "Acosh",
+    "AcoshTensorTransformer",
+    "Asinh",
+    "AsinhTensorTransformer",
+    "Atanh",
+    "AtanhTensorTransformer",
     "BaseTensorTransformer",
     "Clamp",
     "ClampTensorTransformer",
+    "Cosh",
+    "CoshTensorTransformer",
     "Identity",
     "IdentityTensorTransformer",
+    "Sinh",
+    "SinhTensorTransformer",
+    "Tanh",
+    "TanhTensorTransformer",
     "is_tensor_transformer_config",
     "setup_tensor_transformer",
 ]
@@ -25,3 +37,15 @@ from startorch.tensor.transformer.math import AbsTensorTransformer
 from startorch.tensor.transformer.math import AbsTensorTransformer as Abs
 from startorch.tensor.transformer.math import ClampTensorTransformer
 from startorch.tensor.transformer.math import ClampTensorTransformer as Clamp
+from startorch.tensor.transformer.trigo import AcoshTensorTransformer
+from startorch.tensor.transformer.trigo import AcoshTensorTransformer as Acosh
+from startorch.tensor.transformer.trigo import AsinhTensorTransformer
+from startorch.tensor.transformer.trigo import AsinhTensorTransformer as Asinh
+from startorch.tensor.transformer.trigo import AtanhTensorTransformer
+from startorch.tensor.transformer.trigo import AtanhTensorTransformer as Atanh
+from startorch.tensor.transformer.trigo import CoshTensorTransformer
+from startorch.tensor.transformer.trigo import CoshTensorTransformer as Cosh
+from startorch.tensor.transformer.trigo import SinhTensorTransformer
+from startorch.tensor.transformer.trigo import SinhTensorTransformer as Sinh
+from startorch.tensor.transformer.trigo import TanhTensorTransformer
+from startorch.tensor.transformer.trigo import TanhTensorTransformer as Tanh
