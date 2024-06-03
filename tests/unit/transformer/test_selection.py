af8dc9daa1b936629910c0e849912304ecc81bc0
Thibaut Durand <durand.tibo@gmail.com>
2024-06-02 18:44:53 -0700

    Add `RemoveKeysTransformer`

--- tests/unit/transformer/test_selection.py
+++ tests/unit/transformer/test_selection.py
@@ -4,8 +4,112 @@ import pytest
 import torch
 from coola import objects_are_equal
 
-from startorch.transformer import SelectKeysTransformer
+from startorch.transformer import RemoveKeysTransformer, SelectKeysTransformer
 from startorch.utils.seed import get_torch_generator
+
+###########################################
+#     Tests for RemoveKeysTransformer     #
+###########################################
+
+
+def test_remove_keys_transformer_str() -> None:
+    assert str(RemoveKeysTransformer(keys=["input3"])).startswith("RemoveKeysTransformer(")
+
+
+def test_remove_keys_transformer_transform_0_key() -> None:
+    assert objects_are_equal(
+        RemoveKeysTransformer(keys=[]).transform(
+            {
+                "input1": torch.tensor([[0.0, -1.0, 2.0], [-4.0, 5.0, -6.0]]),
+                "input2": torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]),
+                "input3": torch.ones(2, 4),
+            }
+        ),
+        {
+            "input1": torch.tensor([[0.0, -1.0, 2.0], [-4.0, 5.0, -6.0]]),
+            "input2": torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]),
+            "input3": torch.ones(2, 4),
+        },
+    )
+
+
+def test_remove_keys_transformer_transform_1_key() -> None:
+    assert objects_are_equal(
+        RemoveKeysTransformer(keys=["input3"]).transform(
+            {
+                "input1": torch.tensor([[0.0, -1.0, 2.0], [-4.0, 5.0, -6.0]]),
+                "input2": torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]),
+                "input3": torch.randn(2, 4),
+            }
+        ),
+        {
+            "input1": torch.tensor([[0.0, -1.0, 2.0], [-4.0, 5.0, -6.0]]),
+            "input2": torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]),
+        },
+    )
+
+
+def test_remove_keys_transformer_transform_2_keys() -> None:
+    assert objects_are_equal(
+        RemoveKeysTransformer(keys=["input3", "input2"]).transform(
+            {
+                "input1": torch.tensor([[0.0, -1.0, 2.0], [-4.0, 5.0, -6.0]]),
+                "input2": torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]),
+                "input3": torch.randn(2, 4),
+            }
+        ),
+        {
+            "input1": torch.tensor([[0.0, -1.0, 2.0], [-4.0, 5.0, -6.0]]),
+        },
+    )
+
+
+def test_remove_keys_transformer_transform_missing_ok_false() -> None:
+    transformer = RemoveKeysTransformer(keys=["input3", "missing"])
+    with pytest.raises(KeyError, match="missing is missing."):
+        transformer.transform(
+            {
+                "input1": torch.tensor([[0.0, -1.0, 2.0], [-4.0, 5.0, -6.0]]),
+                "input2": torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]),
+                "input3": torch.randn(2, 4),
+            }
+        )
+
+
+def test_remove_keys_transformer_transform_missing_ok_true() -> None:
+    assert objects_are_equal(
+        RemoveKeysTransformer(keys=["input3", "missing"], missing_ok=True).transform(
+            {
+                "input1": torch.tensor([[0.0, -1.0, 2.0], [-4.0, 5.0, -6.0]]),
+                "input2": torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]),
+                "input3": torch.randn(2, 4),
+            }
+        ),
+        {
+            "input1": torch.tensor([[0.0, -1.0, 2.0], [-4.0, 5.0, -6.0]]),
+            "input2": torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]),
+        },
+    )
+
+
+def test_remove_keys_transformer_transform_same_random_seed() -> None:
+    transformer = RemoveKeysTransformer(keys=["input3"])
+    data = {"input1": torch.randn(4, 12), "input2": torch.randn(4, 12), "input3": torch.randn(2, 4)}
+    assert objects_are_equal(
+        transformer.transform(data, rng=get_torch_generator(1)),
+        transformer.transform(data, rng=get_torch_generator(1)),
+    )
+
+
+def test_remove_keys_transformer_transform_different_random_seeds() -> None:
+    transformer = RemoveKeysTransformer(keys=["input3"])
+    data = {"input1": torch.randn(4, 12), "input2": torch.randn(4, 12), "input3": torch.randn(2, 4)}
+    # the outputs must be equal because this transformer does not have randomness
+    assert objects_are_equal(
+        transformer.transform(data, rng=get_torch_generator(1)),
+        transformer.transform(data, rng=get_torch_generator(2)),
+    )
+
 
 ###########################################
 #     Tests for SelectKeysTransformer     #
@@ -85,7 +189,6 @@ def test_select_keys_transformer_transform_missing_ok_true() -> None:
             "input1": torch.tensor([[0.0, -1.0, 2.0], [-4.0, 5.0, -6.0]]),
             "input2": torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]),
         },
-        show_difference=True,
     )
 
 
