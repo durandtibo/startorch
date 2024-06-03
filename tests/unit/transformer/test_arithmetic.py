d825d1dcf84232c69279ad0aabfbe67199971928
Thibaut Durand <durand.tibo@gmail.com>
2024-06-02 17:53:35 -0700

    Add `FmodTransformer` (#463)

--- tests/unit/transformer/test_arithmetic.py
+++ tests/unit/transformer/test_arithmetic.py
@@ -7,6 +7,7 @@ from coola import objects_are_allclose, objects_are_equal
 from startorch.transformer import (
     AddTransformer,
     DivTransformer,
+    FmodTransformer,
     MulTransformer,
     SubTransformer,
 )
@@ -245,6 +246,90 @@ def test_div_transformer_transform_same_random_seed() -> None:
 
 def test_div_transformer_transform_different_random_seeds() -> None:
     transformer = DivTransformer(dividend="input1", divisor="input2", output="output")
+    data = {"input1": torch.randn(4, 12), "input2": torch.randn(4, 12)}
+    # the outputs must be equal because this transformer does not have randomness
+    assert objects_are_equal(
+        transformer.transform(data, rng=get_torch_generator(1)),
+        transformer.transform(data, rng=get_torch_generator(2)),
+    )
+
+
+#####################################
+#     Tests for FmodTransformer     #
+#####################################
+
+
+def test_fmod_transformer_str() -> None:
+    assert str(FmodTransformer(dividend="input1", divisor="input2", output="output")).startswith(
+        "FmodTransformer("
+    )
+
+
+def test_fmod_transformer_transform() -> None:
+    assert objects_are_allclose(
+        FmodTransformer(dividend="input1", divisor="input2", output="output").transform(
+            {
+                "input1": torch.tensor([[0.0, -1.0, 2.0], [-3.0, 5.0, -7.0]]),
+                "input2": torch.tensor([[1.0, 2.0, 2.0], [4.0, 5.0, 6.0]]),
+            }
+        ),
+        {
+            "input1": torch.tensor([[0.0, -1.0, 2.0], [-3.0, 5.0, -7.0]]),
+            "input2": torch.tensor([[1.0, 2.0, 2.0], [4.0, 5.0, 6.0]]),
+            "output": torch.tensor([[0.0, -1.0, 0.0], [-3.0, 0.0, -1.0]]),
+        },
+    )
+
+
+def test_fmod_transformer_transform_exist_ok_false() -> None:
+    data = {
+        "input1": torch.tensor([[0.0, -1.0, 2.0], [-3.0, 5.0, -7.0]]),
+        "input2": torch.tensor([[1.0, 2.0, 2.0], [4.0, 5.0, 6.0]]),
+        "output": 1,
+    }
+    transformer = FmodTransformer(
+        dividend="input1", divisor="input2", output="output", exist_ok=False
+    )
+    with pytest.raises(KeyError, match="Key output already exists."):
+        transformer.transform(data)
+
+
+def test_fmod_transformer_transform_exist_ok_true() -> None:
+    data = {
+        "input1": torch.tensor([[0.0, -1.0, 2.0], [-3.0, 5.0, -7.0]]),
+        "input2": torch.tensor([[1.0, 2.0, 2.0], [4.0, 5.0, 6.0]]),
+        "output": 1,
+    }
+    out = FmodTransformer(
+        dividend="input1", divisor="input2", output="output", exist_ok=True
+    ).transform(data)
+    assert objects_are_equal(
+        out,
+        {
+            "input1": torch.tensor([[0.0, -1.0, 2.0], [-3.0, 5.0, -7.0]]),
+            "input2": torch.tensor([[1.0, 2.0, 2.0], [4.0, 5.0, 6.0]]),
+            "output": torch.tensor([[0.0, -1.0, 0.0], [-3.0, 0.0, -1.0]]),
+        },
+    )
+
+
+def test_fmod_transformer_transform_missing_key() -> None:
+    transformer = FmodTransformer(dividend="input1", divisor="input2", output="output")
+    with pytest.raises(KeyError, match="Missing key: input1."):
+        transformer.transform({})
+
+
+def test_fmod_transformer_transform_same_random_seed() -> None:
+    transformer = FmodTransformer(dividend="input1", divisor="input2", output="output")
+    data = {"input1": torch.randn(4, 12), "input2": torch.randn(4, 12)}
+    assert objects_are_equal(
+        transformer.transform(data, rng=get_torch_generator(1)),
+        transformer.transform(data, rng=get_torch_generator(1)),
+    )
+
+
+def test_fmod_transformer_transform_different_random_seeds() -> None:
+    transformer = FmodTransformer(dividend="input1", divisor="input2", output="output")
     data = {"input1": torch.randn(4, 12), "input2": torch.randn(4, 12)}
     # the outputs must be equal because this transformer does not have randomness
     assert objects_are_equal(
